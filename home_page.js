d8601fef6a8cec831c2d9fa96fc9643f0c5f4076
AnasAA412 <Anasanchu4430@gmail.com>
2024-01-08 21:40:53 +0530

    completed the kerala tourism website using bootstrap.

---
+++ home_page.js
@@ -0,0 +1,74 @@
+// var cards = document.querySelectorAll(".cards");
+// for (var i = 0; i < cards.length; i++) {
+//   cards[i].addEventListener(
+//     "click",
+//     function (e) {
+//       var link = this.querySelector(".place-card");
+//       link.click();
+//     },
+//     false
+//   );
+// }
+
+document.getElementById("ksd").addEventListener("click", function (event) {
+  window.location.href = "./district/kasaragod.html";
+  event.preventDefault();
+});
+
+document.getElementById("knr").addEventListener("click", function (event) {
+  window.location.href = "./district/kannur.html";
+  event.preventDefault();
+});
+document.getElementById("kzkd").addEventListener("click", function (event) {
+  window.location.href = "./district/kozhikode.html";
+  event.preventDefault();
+});
+
+document.getElementById("wynd").addEventListener("click", function (event) {
+  window.location.href = "./district/wayanad.html";
+  event.preventDefault();
+});
+document.getElementById("mlpm").addEventListener("click", function (event) {
+  window.location.href = "./district/malappuram.html";
+  event.preventDefault();
+});
+document.getElementById("plkd").addEventListener("click", function (event) {
+  window.location.href = "./district/palakkad.html";
+  event.preventDefault();
+});
+
+document.getElementById("kochi").addEventListener("click", function (event) {
+  window.location.href = "./district/kochi.html";
+  event.preventDefault();
+});
+document.getElementById("trsur").addEventListener("click", function (event) {
+  window.location.href = "./district/thrissur.html";
+  event.preventDefault();
+});
+
+document.getElementById("idki").addEventListener("click", function (event) {
+  window.location.href = "./district/idukki.html";
+  event.preventDefault();
+});
+document.getElementById("pthita").addEventListener("click", function (event) {
+  window.location.href = "./district/pathanamthitta.html";
+  event.preventDefault();
+});
+document.getElementById("klm").addEventListener("click", function (event) {
+  window.location.href = "./district/kollam.html";
+  event.preventDefault();
+});
+
+document.getElementById("alpzha").addEventListener("click", function (event) {
+  window.location.href = "./district/alappuzha.html";
+  event.preventDefault();
+});
+document.getElementById("ktym").addEventListener("click", function (event) {
+  window.location.href = "./district/kottayam.html";
+  event.preventDefault(0);
+});
+
+document.getElementById("tvm").addEventListener("click", function (event) {
+  window.location.href = "./district/tvm.html";
+  event.preventDefault();
+});
